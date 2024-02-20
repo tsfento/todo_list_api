4a4c75337b64eeaaa4dcd2143c57728dc361cd0f
Tyler Fenton <tsfento@gmail.com>
2024-02-20 13:55:43 -0600

    Finished authentication reading material

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_15_165132) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_20_193428) do
   create_table "todos", force: :cascade do |t|
     t.string "body"
     t.boolean "is_completed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
